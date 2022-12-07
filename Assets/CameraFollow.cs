ee58c5aa348bec1d6c9b7abeca019a67efd868b1
NikThomas9 <nikrthomas@gmail.com>
2022-12-06 20:27:45 -0800

    Camera Tweaks

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform follow;
-    [SerializeField] private float damping = 200;
+    [SerializeField] private float defualtDamping = 200;
+    private float damping = 200;
+
 
     private Camera cam;
 
@@ -23,18 +25,31 @@ public class CameraFollow : MonoBehaviour
 
     public void SwitchTo(Transform newFollow, float newFOV)
     {
+        damping = defualtDamping;
         follow = newFollow;
         StartCoroutine(SwitchFOV(newFOV));
     }
 
     private IEnumerator SwitchFOV(float to)
     {
-        while (cam.fieldOfView < to-0.1 || cam.fieldOfView > to+0.1)
+        while (cam.fieldOfView < to-.5 || cam.fieldOfView > to+.5)
         {
             cam.fieldOfView = (to-cam.fieldOfView)/damping+cam.fieldOfView;
             yield return null;
         }
-        
+
         cam.fieldOfView = to;
+        StartCoroutine(RemoveDampen());
+    }
+
+    private IEnumerator RemoveDampen()
+    {
+        float t = 0;
+        while (damping > 1)
+        {
+            damping = Mathf.Lerp(defualtDamping, 1, t);
+            t += 1f * Time.deltaTime;
+            yield return null;
+        }
     }
 }
